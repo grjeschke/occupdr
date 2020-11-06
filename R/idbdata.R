31e8dd7aa83dd5921c8b9b3cfcc0c26b07f1dca1
Grace Jeschke <grj14@georgetown.edu>
2020-11-06 22:18:42 +0000

    this should be better

--- R/idbdata.R
+++ R/idbdata.R
@@ -7,6 +7,5 @@
 #' @format a data frame with 15 observations and 184 variables
 #' 
 #' @source /url{http://www.idigbio.org/portal} (2020),
-#' Query: {"filtered": {"filter": {"and": [{"query": {"match": {"_all": {"operator": "and", "query": "vanessa cardui"}}}}]}}},
 #' 3427 records, accessed on 2020-10-27T19:38:47.660127
 "idbdata"
